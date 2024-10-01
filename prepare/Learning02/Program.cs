f7085046f7650046ca5efa668a48ef712c854894
Atubi Gift <giftatubi39@gmail.com>
2024-10-01 09:04:33 +0100

    Added Resume and Job to my Csharp program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle="Software Engineer";
+        job1._company="Microsoft";
+        job1._startYear=2001;
+        job1._endYear=2008;
+
+        Job job2 = new Job();
+        job2._jobTitle="Software Engineer";
+        job2._company="apple";
+        job2._startYear=2001;
+        job2._endYear=2008;
+
+        job1.DisplayJobDetails(); 
+        job2.DisplayJobDetails(); 
+
+        Resume myResume= new Resume();
+        myResume._name="Gift Atubi";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    
     }
 }
